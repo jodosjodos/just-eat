f0ba6f7c16d78fa7fea2b993a0f5b0518bfa5900
jodosjodos <jeandedieu2030@gmail.com>
2024-09-23 05:49:39 +0200

    [init] init project

---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center"></SafeAreaView>
+  );
+}
