0ef6ad7f4221c8c2d1fbed09b504f7bb930b1332
jodosjodos <jeandedieu2030@gmail.com>
2024-10-04 07:16:25 +0200

    [fix] fix cart ui

--- components/restaurant/Food-item-cart.tsx
+++ components/restaurant/Food-item-cart.tsx
@@ -15,14 +15,14 @@ const FoodDetailsCart: React.FC<FoodITemProp> = ({ foodItem }) => {
         source={foodItem.smallImage}
         resizeMode="cover"
       />
-      <View>
+      <View className=" flex flex-col space-y-2">
         <Link
           href={`/(tabs)/restaurants/food-item/${foodItem.id}`}
           className=" font-kadwa-bold text-primary underline text-lg"
         >
           {foodItem.name}
         </Link>
-        <View className="flex flex-row items-center justify-between mr-12">
+        <View className="flex flex-row items-center justify-between">
           <View className="flex flex-row  items-center space-x-4">
             <Text className="  text-xl font-lekton">${foodItem.price}</Text>
             <Text className="text-xl font-lekton ">
@@ -31,16 +31,22 @@ const FoodDetailsCart: React.FC<FoodITemProp> = ({ foodItem }) => {
           </View>
           <Text className="font-lekton-bold text-xl text-primary">x3 </Text>
         </View>
-        <View className=" w-full flex flex-row items-center  justify-around">
+        <View className="flex flex-row items-center  space-x-12 w-full ">
           <Text className="font-adamina bg-primary text-white py-1 px-2 rounded-lg w-10">
             {foodItem.rating}
           </Text>
-
           <TouchableOpacity
-            className="bg-secondary px-5 py-1 rounded-md  flex items-center justify-center"
+            // className="bg-secondary px-5 py-1 rounded-md  flex items-center justify-center"
+            className=""
             onPress={orderFood}
           >
-            <Text className="font- text-xl">Order</Text>
+            <Text className="font-lekton text-end text-xl text-red-400 underline">remove</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            // className="bg-secondary px-5 py-1 rounded-md  flex items-center justify-center"
+            onPress={orderFood}
+          >
+            <Text className="font-lekton-bold text-end text-xl text-primary underline">Order</Text>
           </TouchableOpacity>
         </View>
       </View>
