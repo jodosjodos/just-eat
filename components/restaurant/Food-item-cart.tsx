a461ed8a270c1b05c2c96fc3babe7c10c48af8eb
jodosjodos <jeandedieu2030@gmail.com>
2024-10-04 07:06:31 +0200

    [fix] show time it will be ordered

--- components/restaurant/Food-item-cart.tsx
+++ components/restaurant/Food-item-cart.tsx
@@ -7,8 +7,7 @@ interface FoodITemProp {
 }
 //TODO:handle way to move food from cart to order page
 const FoodDetailsCart: React.FC<FoodITemProp> = ({ foodItem }) => {
-  const orderFood = () => {
-  };
+  const orderFood = () => {};
   return (
     <View className="flex flex-row  hover:cursor-pointer px-6 space-x-2 py-3 border-b border-secondary w-full">
       <Image
@@ -23,9 +22,14 @@ const FoodDetailsCart: React.FC<FoodITemProp> = ({ foodItem }) => {
         >
           {foodItem.name}
         </Link>
-        <View className="flex flex-row  items-center space-x-4">
-          <Text className="  text-xl font-lekton">${foodItem.price}</Text>
-          <Text className="text-xl font-lekton">.{foodItem.deliveryPrice}</Text>
+        <View className="flex flex-row items-center justify-between mr-12">
+          <View className="flex flex-row  items-center space-x-4">
+            <Text className="  text-xl font-lekton">${foodItem.price}</Text>
+            <Text className="text-xl font-lekton ">
+              .{foodItem.deliveryPrice}
+            </Text>
+          </View>
+          <Text className="font-lekton-bold text-xl text-primary">x3 </Text>
         </View>
         <View className=" w-full flex flex-row items-center  justify-around">
           <Text className="font-adamina bg-primary text-white py-1 px-2 rounded-lg w-10">
