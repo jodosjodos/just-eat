3b4e9fb4468f15bb1261495f1b6c30977abe6587
jodosjodos <jeandedieu2030@gmail.com>
2024-10-05 10:29:09 +0200

    [add] done with payment type page

--- app/(tabs)/restaurants/payment/index.tsx
+++ app/(tabs)/restaurants/payment/index.tsx
@@ -43,46 +43,50 @@ const PaymentPage = () => {
   }, []);
   return (
     <SafeAreaView className="bg-white">
-      <ScrollView className="h-full w-full flex flex-col space-y-8">
-        <View className="flex flex-row items-center px-3">
-          <Image
-            source={
-              paymentMethod === "cash" ? images.checked : images.unChecked
-            }
-            className={` absolute z-50  ${
-              paymentMethod === "cash" ? "top-2" : "top3"
-            } left-5 w-8 h-8`}
-            resizeMode="cover"
-          />
-          <View
-            className=" w-full py-4 rounded-lg   bg-secondary border-none pl-12"
-            style={styles.shadowCustom}
-          >
-            <Pressable onPress={() => setPaymentMethod("cash")}>
-              <Text className="font-lekton text-lg">Cash On delivery</Text>
-            </Pressable>
+      <ScrollView className=" w-full h-full flex flex-col space-y-5">
+     
+        <View className="flex flex-col space-y-3">
+          <View className="flex flex-row items-center px-3">
+            <Image
+              source={
+                paymentMethod === "cash" ? images.checked : images.unChecked
+              }
+              className={` absolute z-50  ${
+                paymentMethod === "cash" ? "top-2" : "top3"
+              } left-5 w-8 h-8`}
+              resizeMode="cover"
+            />
+            <View
+              className=" w-full py-4 rounded-lg   bg-secondary border-none pl-12"
+              style={styles.shadowCustom}
+            >
+              <Pressable onPress={() => setPaymentMethod("cash")}>
+                <Text className="font-lekton text-lg">Cash On delivery</Text>
+              </Pressable>
+            </View>
+          </View>
+
+          <View className="flex flex-row items-center px-3">
+            <Image
+              source={
+                paymentMethod === "online" ? images.checked : images.unChecked
+              }
+              className={` absolute z-50  ${
+                paymentMethod === "online" ? "top-2" : "top3"
+              } left-5 w-8 h-8`}
+              resizeMode="cover"
+            />
+            <View
+              className=" w-full py-4 rounded-lg   bg-secondary border-none pl-12"
+              style={styles.shadowCustom}
+            >
+              <Pressable onPress={() => setPaymentMethod("online")}>
+                <Text className="font-lekton text-lg">Online payment</Text>
+              </Pressable>
+            </View>
           </View>
         </View>
 
-        <View className="flex flex-row items-center px-3">
-          <Image
-            source={
-              paymentMethod === "online" ? images.checked : images.unChecked
-            }
-            className={` absolute z-50  ${
-              paymentMethod === "online" ? "top-2" : "top3"
-            } left-5 w-8 h-8`}
-            resizeMode="cover"
-          />
-          <View
-            className=" w-full py-4 rounded-lg   bg-secondary border-none pl-12"
-            style={styles.shadowCustom}
-          >
-            <Pressable onPress={() => setPaymentMethod("online")}>
-              <Text className="font-lekton text-lg">Online payment</Text>
-            </Pressable>
-          </View>
-        </View>
         <View className="flex flex-col space-y-2 px-12">
           <View className="flex flex-row justify-between mt-3">
             <Text className="font-kadwa text-xl">Sub Total</Text>
@@ -145,7 +149,7 @@ const PaymentPage = () => {
             </View>
           </View>
         )}
-        <TouchableOpacity className="bg-primary  rounded-full items-center  py-5 px-5 mx-3">
+        <TouchableOpacity className="bg-primary  rounded-full items-center  py-4 px-5 mx-3 mb-8">
           <Text className="text-white font-adamina text-2xl ">DONE</Text>
         </TouchableOpacity>
       </ScrollView>
