a0f84b904049adebf7e2b85e4311b1b4794232b5
jodosjodos <jeandedieu2030@gmail.com>
2024-10-07 10:06:18 +0200

    [add] doen with rating page

--- app/(tabs)/restaurants/order-status/rating.tsx
+++ app/(tabs)/restaurants/order-status/rating.tsx
@@ -1,6 +1,6 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
-import { useNavigation } from "expo-router";
+import { Link, useNavigation } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Image } from "react-native";
@@ -24,14 +24,16 @@ const Rating: React.FC = () => {
   };
 
   return (
-    <SafeAreaView style={{ backgroundColor: "white" }}>
-      <View style={{ height: "100%", width: "100%" }}>
-        <Text>Skip</Text>
-        <View>
-          <View>
+    <SafeAreaView className="bg-white">
+      <View className="h-full w-full space-y-5">
+        <Link href="/(tabs)/home" className="font-lekton text-xl  pl-8 pt-4">
+          Skip
+        </Link>
+        <View className="flex  items-center justify-center border-b-2 border-secondary pb-6">
+          <View className="flex flex-col  space-y-3">
             <Image source={images.userProfile2} />
-            <View>
-              <Text>JOHN</Text>
+            <View className="flex flex-row items-center justify-between space-x-3">
+              <Text className="font-kadwa-bold self-center text-xl">JOHN</Text>
               <View className="flex flex-row items-center">
                 {Array.from({ length: 4 }, (_, index) => (
                   <TouchableOpacity
@@ -47,15 +49,17 @@ const Rating: React.FC = () => {
                 ))}
               </View>
             </View>
-            <TouchableOpacity className="bg-primary  rounded-full items-center  py-5 px-8 my-6">
-              <Text className="text-white font-adamina text-2xl ">Submit</Text>
+            <TouchableOpacity className="bg-primary  rounded-full items-center  py-2  my-6">
+              <Text className="text-white font-adamina text-xl ">Submit</Text>
             </TouchableOpacity>
           </View>
         </View>
-        <View>
+        <View className="flex flex-col items-center justify-center space-y-4">
           <Image source={images.logoRating} />
-          <Text>Just EAT</Text>
-          <Text>Thank you for your order</Text>
+          <Text className="font-korean-medium text-primary text-3xl">
+            Just EAT
+          </Text>
+          <Text className="font-kadwa text-2xl">Thank you for your order</Text>
         </View>
       </View>
     </SafeAreaView>
