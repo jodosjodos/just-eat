bae54ac11a4c0fe78b25e3cdac27a7885443754e
jodosjodos <jeandedieu2030@gmail.com>
2024-10-12 09:44:30 +0200

    [fix] fixx

--- app/(tabs)/restaurants/payment/index.tsx
+++ app/(tabs)/restaurants/payment/index.tsx
@@ -14,6 +14,7 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { styles } from "@/constants/styles";
 import { images } from "@/constants";
 import { useToast } from "react-native-toast-notifications";
+//TODO: implementing stripe
 const PaymentPage = () => {
   const navigation = useNavigation();
   const toast = useToast();
